8e98bc53a8d8f5f062541e2666a12927a76e258e
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-13 14:21:23 -0700

    customers from postgreSQL load and render

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Customers from './components/customers/Customers'
 
 function App() {
+  const [customerData, setCustomerData] = useState([])
+
+  useEffect(() => {
+    axios('http://localhost:5000/api/customers')
+      .then(customers => {
+        setCustomerData(customers.data.data)
+      })
+      .then(err => console.log(`unable to retrieve customer data`, err))
+  }, [])
+
   return (
     <div className="App">
-      
+      {/* <button onClick={() => console.log(customerData)}>log data</button> */}
+      <Customers customers={customerData} />
     </div>
   );
 }
