e5101e07b66019e8b43b13e858241123f143a915
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-13 14:24:21 -0700

    save

--- api/server.js
+++ api/server.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require("express");
 const db = require("../data/dbConfig.js");
 const server = express();
+const cors = require('cors');
 
+server.use(cors())
 server.use(express.json());
 
 module.exports = server;
