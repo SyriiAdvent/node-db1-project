68606cd33410a8a8dd1fd149d5fbde24b6a1bf0d
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-13 13:43:52 -0700

    PostgreSQL CRUDS working on customers table

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require("./api/server.js");
 const PORT = process.env.PORT || 5000;
 const accountsRouter = require('./api/accounts/accountsRouter');
+const customersRouter = require('./api/customers/customersRouter');
 
 server.use('/api/accounts', accountsRouter)
+server.use('/api/customers', customersRouter)
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
